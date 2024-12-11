3a8f5da029747d89d5531f4747aaddd9506c2e31
Nicolas Frere <107085649+Gotlub@users.noreply.github.com>
2024-12-11 16:53:11 +0100

    Create FordFulkerson.py

---
+++ semaine7/FordFulkerson.py
@@ -0,0 +1,114 @@
+# Python program for implementation 
+# of Ford Fulkerson algorithm
+from collections import defaultdict
+ 
+# This class represents a directed graph 
+# using adjacency matrix representation
+class Graph:
+ 
+    def __init__(self, graph):
+        self.graph = graph  # residual graph
+        self. ROW = len(graph)
+        # self.COL = len(gr[0])
+ 
+    '''Returns true if there is a path from source 's' to sink 't' in
+    residual graph. Also fills parent[] to store the path '''
+ 
+    def BFS(self, s, t, parent):
+ 
+        # Mark all the vertices as not visited
+        visited = [False]*(self.ROW)
+ 
+        # Create a queue for BFS
+        queue = []
+ 
+        # Mark the source node as visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+ 
+         # Standard BFS Loop
+        while queue:
+ 
+            # Dequeue a vertex from queue and print it
+            u = queue.pop(0)
+ 
+            # Get all adjacent vertices of the dequeued vertex u
+            # If a adjacent has not been visited, then mark it
+            # visited and enqueue it
+            for ind, val in enumerate(self.graph[u]):
+                if visited[ind] == False and val > 0:
+                      # If we find a connection to the sink node, 
+                    # then there is no point in BFS anymore
+                    # We just have to set its parent and can return true
+                    queue.append(ind)
+                    visited[ind] = True
+                    parent[ind] = u
+                    if ind == t:
+                        return True
+ 
+        # We didn't reach sink in BFS starting 
+        # from source, so return false
+        return False
+             
+     
+    # Returns the maximum flow from s to t in the given graph
+    def FordFulkerson(self, source, sink):
+ 
+        # This array is filled by BFS and to store path
+        parent = [-1]*(self.ROW)
+ 
+        max_flow = 0 # There is no flow initially
+        bfsNumber = 0
+        # Augment the flow while there is path from source to sink
+        while self.BFS(source, sink, parent) :
+ 
+            # Find minimum residual capacity of the edges along the
+            # path filled by BFS. Or we can say find the maximum flow
+            # through the path found.
+            path_flow = float("Inf")
+            s = sink
+            parents = [s]
+            while(s !=  source):
+                path_flow = min (path_flow, self.graph[parent[s]][s])
+                s = parent[s]
+                parents += [s]
+ 
+            # Add path flow to overall flow
+            max_flow +=  path_flow
+
+            print("Pass " + str(bfsNumber) + " way  : " + "-".join(str(k) for k in parents[::-1]) + " with a flow of : " + str(path_flow))
+            # update residual capacities of the edges and reverse edges
+            # along the path
+            v = sink
+            while(v !=  source):
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+            bfsNumber += 1
+
+        return max_flow
+ 
+  
+# Create a graph given in the above diagram
+print() 
+graph = [[0, 16, 13, 0, 0, 0],
+        [0, 0, 10, 12, 0, 0],
+        [0, 4, 0, 0, 14, 0],
+        [0, 0, 9, 0, 0, 20],
+        [0, 0, 0, 7, 0, 4],
+        [0, 0, 0, 0, 0, 0]]
+copieGraph = [[ graph[k][l] for l in range(len(graph))]for k in range(len(graph))]
+g = Graph(graph)
+source = 0; sink = 5
+#faire une copie du graph, soustraire les positif de base - les meme niveau result = valeur des arcs  
+print ("The maximum possible flow is %d " % g.FordFulkerson(source, sink))
+#the subtraction from the original matrix, and from that modified by the algorithm allows to have a graph of the max flow
+result = [[copieGraph[i][j] - graph[i][j] if copieGraph[i][j] != 0 else 0 for j in range(len(graph))] for i in range(len(graph))]
+#just to have a clean display
+tailleMax = 0
+for k in result : tailleMax = max(tailleMax, max(k))
+
+print("Matrice resultat : ")
+for k in range(len(graph)):
+    print(" ".join(str(l) + ' ' * max(0 , (len(str(tailleMax)) - len(str(l)))) for l in result[k]))
